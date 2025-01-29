1ff7962b88d605357d0fcf55ed2f23d50c844854
dubois <ximen.dubois@student.howest.be>
2025-01-29 19:16:59 +0100

    Pie chard animatie en Andere verhalen sectie

--- script.js
+++ script.js
@@ -1,91 +1,89 @@
-
-
 const init = () => {
-
-    const $navButton = document.querySelector('.nav__button');
-    const $navList = document.querySelector('.nav__list');
-    const $navSmall = document.querySelector('.nav-small_hidden');
-    const $navMenu = document.querySelector('.menu');
-    const $iconLink = document.querySelector('#iconlink');
-    const listItems = $navList.querySelectorAll("li a");
-    const $navSmallElements = document.querySelectorAll('.nav-small_open');
-    const $polyglotBijbel = document.querySelector('.polyglot-bijbel_image');
-    const $netwerkContainer = document.querySelector('.grid-netwerkAnimatie');
-
-
-    console.log($polyglotBijbel);
-
-    const hambergerMediaQuery = window.matchMedia("(max-width: 60em)");
-    const handleMediaQuery = (event) => {
-        if (event.matches) {
-            $navButton.classList.remove('hidden');
-            $navSmall.classList.remove('hidden');
-            $navMenu.classList.add('hidden');
-            $polyglotBijbel.classList.add('hidden');
-        } else {
-            $navButton.classList.add('hidden');
-            $navMenu.classList.remove('hidden');
-            $navSmall.classList.add('hidden');
-            $polyglotBijbel.classList.remove('hidden');
-            $netwerkContainer.classList.add('grid-netwerkAnimatie')
-        }
-    };
-
     handleMediaQuery(hambergerMediaQuery);
     hambergerMediaQuery.addEventListener('change', handleMediaQuery);
-
-    const openNavigation = () => {
-        $navButton.setAttribute("aria-expanded", "true");
-        $iconLink.setAttribute("xlink:href", "#close");
-        $navMenu.classList.remove("hidden");
-        $navSmallElements.forEach(element => {
-            element.classList.add("hidden");
-        });
-    }
-
-    const closeNavigation = () => {
-        $navButton.setAttribute("aria-expanded", "false");
-        $iconLink.setAttribute("xlink:href", "#navicon");
-        $navMenu.classList.add("hidden");
-        $navSmallElements.forEach(element => {
-            element.classList.remove("hidden");
-        });
-    }
-
-    const toggleNavigation = () => {
-        const open = $navButton.getAttribute("aria-expanded");
-        open === "false" ? openNavigation() : closeNavigation();
-    }
-
-
-    const handleBlur = () => {
-        //if (!event.relatedTarget || !$navList.contains(event.relatedTarget)) {
-        closeNavigation();
-        //}
-    }
-
     $navButton.addEventListener("click", toggleNavigation);
-
-    // add event to the last item in the nav list to trigger the disclosure to close if the user tabs out of the disclosure
     listItems[listItems.length - 1].addEventListener("blur", handleBlur);
-
-    // Close the disclosure if a user presses the escape key
-    window.addEventListener("keyup", (e) => {
-        if (e.key === "Escape") {
-            $navButton.focus();
-            closeNavigation();
-        }
+    window.addEventListener("keyup", handleEscapeKey);
+    updateHiddenState();
+    window.addEventListener('resize', updateHiddenState);
+    allDTs.forEach(dt => {
+        dt.addEventListener('click', toggleDD);
     });
-}
+};
+
+const $navButton = document.querySelector('.nav__button');
+const $navList = document.querySelector('.nav__list');
+const $navSmall = document.querySelector('.nav-small_hidden');
+const $navMenu = document.querySelector('.menu');
+const $iconLink = document.querySelector('#iconlink');
+const listItems = $navList.querySelectorAll("li a");
+const $navSmallElements = document.querySelectorAll('.nav-small_open');
+const $polyglotBijbel = document.querySelector('.polyglot-bijbel_image');
+const $netwerkContainer = document.querySelector('.grid-netwerkAnimatie');
+
+console.log($polyglotBijbel);
+
+const hambergerMediaQuery = window.matchMedia("(max-width: 60em)");
+
+const handleMediaQuery = (event) => {
+    if (event.matches) {
+        $navButton.classList.remove('hidden');
+        $navSmall.classList.remove('hidden');
+        $navMenu.classList.add('hidden');
+        $polyglotBijbel.classList.add('hidden');
+    } else {
+        $navButton.classList.add('hidden');
+        $navMenu.classList.remove('hidden');
+        $navSmall.classList.add('hidden');
+        $polyglotBijbel.classList.remove('hidden');
+        $netwerkContainer.classList.add('grid-netwerkAnimatie');
+    }
+};
+
+const openNavigation = () => {
+    $navButton.setAttribute("aria-expanded", "true");
+    $iconLink.setAttribute("xlink:href", "#close");
+    $navMenu.classList.remove("hidden");
+    $navSmallElements.forEach(element => {
+        element.classList.add("hidden");
+    });
+};
+
+const closeNavigation = () => {
+    $navButton.setAttribute("aria-expanded", "false");
+    $iconLink.setAttribute("xlink:href", "#navicon");
+    $navMenu.classList.add("hidden");
+    $navSmallElements.forEach(element => {
+        element.classList.remove("hidden");
+    });
+};
+
+const toggleNavigation = () => {
+    const open = $navButton.getAttribute("aria-expanded");
+    open === "false" ? openNavigation() : closeNavigation();
+};
+
+const handleBlur = () => {
+    closeNavigation();
+};
+
+const handleEscapeKey = (e) => {
+    if (e.key === "Escape") {
+        $navButton.focus();
+        closeNavigation();
+    }
+};
+
 ///////Enveloppe en Postcard
 const $enveloppe = document.querySelector('.enveloppe');
 const $postcard = document.querySelector('.postcard');
+
 $enveloppe.addEventListener('click', function () {
     $enveloppe.classList.add("hidden");
     $postcard.classList.remove('hidden');
 });
 
-///////lettertype veranderen bij PolyGlot Bijbel
+///////Lettertype veranderen bij PolyGlot Bijbel
 const styles = [
     { font: 'Roboto, sans-serif', text: 'Hebreeuwse Typo' },
     { font: '"Playfair Display", serif', text: 'Griekse Typo' },
@@ -95,17 +93,15 @@ const styles = [
 ];
 
 let currentFontIndex = 0;
-
 const textElement = document.querySelector('.polyglotBijbel-text');
 const button = document.getElementById('changeFont-Btn');
 
 button.addEventListener('click', () => {
     currentFontIndex = (currentFontIndex + 1) % styles.length;
-
     textElement.style.fontFamily = styles[currentFontIndex].font;
-
     button.textContent = styles[currentFontIndex].text;
 });
+
 ////////////Age Slider
 const sentences = {
     4: "Ik",
@@ -153,46 +149,55 @@ function toggleDD(event) {
     });
 }
 
-// Event listener voor de schermgrootte
-window.addEventListener('resize', updateHiddenState);
-
 // Event listener voor klikken op <dt> of <h4>
 const allDTs = document.querySelectorAll('.footer_list dt');
-allDTs.forEach(dt => {
-    dt.addEventListener('click', toggleDD);
-});
 
-// Initialiseer de hidden klasse bij paginalaad
-updateHiddenState();
+////////Lottie Animatie
 
-
-////////lottie animatie
+gsap.registerPlugin(ScrollTrigger);
 let mm = gsap.matchMedia();
 
-const animation = lottie.loadAnimation({
+const pieChardAnimation = lottie.loadAnimation({
+    container: document.querySelector('.pie-chard'),
+    renderer: 'svg',
+    autoplay: false,
+    loop: false,
+    path: 'src/assets/lottie/Pie_Chard.json'
+});
+
+ScrollTrigger.create({
+    trigger: ".grid-container_piechard",
+    markers: true,
+    start: "top 50%",
+    once: true,
+    onEnter: () => {
+        pieChardAnimation.play();
+    }
+});
+
+const netwerkAnimatie = lottie.loadAnimation({
     container: document.querySelector('.lottie-container'),
     renderer: 'svg',
     autoplay: false,
     path: 'src/assets/lottie/PlantijnNetwerkFinalAnimation.json'
 });
 
-gsap.registerPlugin(ScrollTrigger);
-let lottieScrollTrigger = ScrollTrigger.create({
+const netwerkAnimatieScrollTrigger = ScrollTrigger.create({
     trigger: ".lottie-container",
     scrub: true,
-    markers: true,
     start: "top top",
     end: "top+=2000 top",
     pin: true,
     onUpdate: function (self) {
         const progress = self.progress;
-        animation.goToAndStop(animation.totalFrames * progress, true);
+        netwerkAnimatie.goToAndStop(netwerkAnimatie.totalFrames * progress, true);
     },
 });
-mm.add("(max-width: 60em)", () => {
-    lottieScrollTrigger.kill();
-    lottieScrollTrigger = null;
-    animation.play();
 
+mm.add("(max-width: 60em)", () => {
+    netwerkAnimatieScrollTrigger.kill();
+    netwerkAnimatie.play();
 });
+
+
 init();
