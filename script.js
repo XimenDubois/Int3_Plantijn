43d8d27876d2cf65f9b2a3dcb92acb37ec7b4386
dubois <ximen.dubois@student.howest.be>
2025-01-31 11:52:54 +0100

    Aanpassingen deploy en paden

--- script.js
+++ script.js
@@ -31,28 +31,28 @@ let huidigeSchoonzoonIndex = 0;
 const schoonZonen = [
     {
         naam: "Joannes",
-        afbeelding: "./public/assets/img/Joannes_Verhoeven-w_large.avif",
+        afbeelding: "/assets/img/Joannes_Verhoeven-w_large.avif",
         iq: "65%",
         taalvaardigheid: "50%",
         zakelijkInzicht: "40%",
     },
     {
         naam: "Jan",
-        afbeelding: "./public/assets/img/Jan_Marryme-w_large.avif",
+        afbeelding: "assets/img/Jan_Marryme-w_large.avif",
         iq: "85%",
         taalvaardigheid: "90%",
         zakelijkInzicht: "96%",
     },
     {
         naam: "Lisa",
-        afbeelding: "./public/assets/img/Mona_Lisa-w_large.avif",
+        afbeelding: "src/assets/img/Mona_Lisa-w_large.avif",
         iq: "95%",
         taalvaardigheid: "50%",
         zakelijkInzicht: "15%",
     },
     {
         naam: "Adriaan",
-        afbeelding: "./public/assets/img/Adriaan-w_large.avif",
+        afbeelding: "src/assets/img/Adriaan-w_large.avif",
         iq: "55%",
         taalvaardigheid: "70%",
         zakelijkInzicht: "30%",
@@ -252,11 +252,11 @@ const netwerkAnimatie = lottie.loadAnimation({
     container: document.querySelector('.lottie-container'),
     renderer: 'svg',
     autoplay: false,
-    path: './lottie/PlantijnNetwerkFinalAnimation.json'
+    path: '/lottie/PlantijnNetwerkFinalAnimation.json'
 });
 
 const netwerkAnimatieScrollTrigger = ScrollTrigger.create({
-    trigger: "./public/lottie-container",
+    trigger: ".lottie-container",
     scrub: true,
     start: "top top",
     pin: true,
@@ -270,7 +270,7 @@ const pieChardAnimation = lottie.loadAnimation({
     renderer: 'svg',
     autoplay: false,
     loop: false,
-    path: './public/lottie/Pie_Chard.json'
+    path: '/lottie/Pie_Chard.json'
 });
 
 ScrollTrigger.create({
